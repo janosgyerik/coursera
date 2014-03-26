55f54fbb14afec0b4bc9f4ea0efbbc94f1a85a1d
Janos Gyerik <janos@kronos>
2014-03-26 08:16:47 +0100

    can show photo and play back audio

--- android/workbench/iRemember/src/main/java/edu/vuum/mocca/ui/story/StoryViewFragment.java
+++ android/workbench/iRemember/src/main/java/edu/vuum/mocca/ui/story/StoryViewFragment.java
@@ -226,7 +226,7 @@ public class StoryViewFragment extends Fragment {
 			// as a ringtone and play it back as such. Use the RingtonManager function getRingtone on
 			// the audioLinkPath to create the ringtone
 			
-			final Ringtone ringtone = null;
+			final Ringtone ringtone = RingtoneManager.getRingtone(getActivity(), Uri.parse(audioLinkPath));
 			
 			
 			audioButton.setOnClickListener(new OnClickListener() {
@@ -234,9 +234,8 @@ public class StoryViewFragment extends Fragment {
 				public void onClick(View v) {
 					
 					// TODO - Play the ringtone
-					
+                    ringtone.play();
 
-					
 				}
 			});
 			
@@ -274,9 +273,8 @@ public class StoryViewFragment extends Fragment {
 			
 			// TODO - Set the URI of the ImageView to the image path stored in the string
 			// imageMetaDataPath, using the setImageURI function from the ImageView
-			
-			
-			
+            imageMetaDataView.setImageURI(Uri.parse(imageMetaDataPath));
+
 			Long time = Long.valueOf(storyData.storyTime);
 			storyTimeTV.setText(StoryData.FORMAT.format(time));
 			
