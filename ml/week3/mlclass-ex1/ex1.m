a440ba4b40a65854885df052af31289eef7d2344
Janos Gyerik <janos@kronos>
2014-03-30 23:38:34 +0200

    commented out all plotting and pausing

--- ml/week3/mlclass-ex1/ex1.m
+++ ml/week3/mlclass-ex1/ex1.m
@@ -32,8 +32,8 @@ fprintf('Running warmUpExercise ... \n');
 fprintf('5x5 Identity Matrix: \n');
 warmUpExercise()
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% ======================= Part 2: Plotting =======================
@@ -44,10 +44,10 @@ m = length(y); % number of training examples
 
 % Plot Data
 % Note: You have to complete the code in plotData.m
-plotData(X, y);
+%plotData(X, y);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =================== Part 3: Gradient descent ===================
 fprintf('Running Gradient Descent ...\n')
@@ -70,10 +70,10 @@ fprintf('Theta found by gradient descent: ');
 fprintf('%f %f \n', theta(1), theta(2));
 
 % Plot the linear fit
-hold on; % keep previous plot visible
-plot(X(:,2), X*theta, '-')
-legend('Training data', 'Linear regression')
-hold off % don't overlay any more plots on this figure
+%hold on; % keep previous plot visible
+%plot(X(:,2), X*theta, '-')
+%legend('Training data', 'Linear regression')
+%hold off % don't overlay any more plots on this figure
 
 % Predict values for population sizes of 35,000 and 70,000
 predict1 = [1, 3.5] *theta;
@@ -83,8 +83,8 @@ predict2 = [1, 7] * theta;
 fprintf('For population = 70,000, we predict a profit of %f\n',...
     predict2*10000);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% ============= Part 4: Visualizing J(theta_0, theta_1) =============
 fprintf('Visualizing J(theta_0, theta_1) ...\n')
@@ -109,14 +109,14 @@ end
 % transpose J_vals before calling surf, or else the axes will be flipped
 J_vals = J_vals';
 % Surface plot
-figure;
-surf(theta0_vals, theta1_vals, J_vals)
-xlabel('\theta_0'); ylabel('\theta_1');
+%figure;
+%surf(theta0_vals, theta1_vals, J_vals)
+%xlabel('\theta_0'); ylabel('\theta_1');
 
 % Contour plot
-figure;
+%figure;
 % Plot J_vals as 15 contours spaced logarithmically between 0.01 and 100
-contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
-xlabel('\theta_0'); ylabel('\theta_1');
-hold on;
-plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
+%contour(theta0_vals, theta1_vals, J_vals, logspace(-2, 3, 20))
+%xlabel('\theta_0'); ylabel('\theta_1');
+%hold on;
+%plot(theta(1), theta(2), 'rx', 'MarkerSize', 10, 'LineWidth', 2);
